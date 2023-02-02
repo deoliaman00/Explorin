2bb05c8be048274b8e6eba46ac710c7ef9069364
deoliaman00 <deoliaman00@gmail.com>
2023-02-02 17:50:06 +0530

    Added all the requirements of a post

--- Django Project/blog/blogpart/views.py
+++ Django Project/blog/blogpart/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-
-posts=[
-    {
-        'title':'Aman Deoli',
-        'tag': 'Military',
-        'words':'50'
-    },
-    {
-        'title':'Naman Defu',
-        'tag': 'Airforce',
-        'words':'52'
-    }
-]
 
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blogpart/home.html',context)
 
